6782771d7e3a7704a0d9c3b971f415be6e56dc56
Aaron Claes <aaronclaes22@gmail.com>
2020-11-14 18:02:19 +0100

    making the navigation work

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+//Router
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route>
+          <ContactUs path="/contact" />
+        </Route>
+      </Switch>
     </div>
   );
 }
