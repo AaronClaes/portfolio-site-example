2753c78c164caa33c9d451e20aff7cb9d35f4605
Aaron Claes <aaronclaes22@gmail.com>
2020-11-17 20:02:36 +0100

    added animations to our work page

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,11 +3,17 @@ import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
-
+import { useScroll } from "./useScroll";
+import { fade, scrollReveal } from "../animation";
 const FaqSection = () => {
-  const [faqToggle, setFaqToggle] = useState(false);
+  const [element, controls] = useScroll();
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
